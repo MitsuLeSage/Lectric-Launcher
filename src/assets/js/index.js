017095c7fbb746100cc096e7aead41c9fb447b03
Mimitsu_ <90351357+MitsuLeSage@users.noreply.github.com>
2023-07-13 05:21:12 +0200

    1.0.3

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Mitsu" },
+            { "message": "Salut je suis du code.", "author": "Mitsu" },
+            { "message": "La baleine bleu pèse de 130 000 a 150 000 kg.", "author": "Mitsu" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
